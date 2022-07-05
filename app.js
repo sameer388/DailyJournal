bdd5343418a1c47a2b16a78e9be19dbcef88c5ae
Sameer <sameerpandey388@gmail.com>
2022-07-06 00:28:19 +0530

    Add the procfile and change the port

--- app.js
+++ app.js
@@ -63,7 +63,10 @@ app.post("/compose", function (req, res) {
    });
 
 })
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(){
+  console.log("Server has started successfully.");
 });
